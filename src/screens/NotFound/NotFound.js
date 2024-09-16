9c6a7fbe29e014bfc9e2be95ce28a2a722d2754d
melimarquezz <mmelina@udesa.edu.ar>
2024-09-16 15:45:03 -0300

    Creación página Not Found

---
+++ src/screens/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+// linkear header y footer
+
+function NotFound() {
+    return(
+        
+            <h1> 404 Página no encontrada</h1>
+    )
+}
+
+export default NotFound;
