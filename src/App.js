31fa5c415d729cebd4924c05905bc9365dc3e843
melimarquezz <mmelina@udesa.edu.ar>
2024-09-16 15:52:55 -0300

    Creación página Not Found parte 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
+import NotFound from './screens/NotFound/NotFound'; 
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Switch>
+    <Route path="" component= {NotFound}/>
+    </Switch>
   );
 }
+
 
 export default App;
